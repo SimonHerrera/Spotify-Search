2d9f612facdabf5482444b4704da0ce72317f3f1
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-12 18:30:48 -0600

    Added Navbar, Search and About files

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { SearchComponent }  from './components/search/search.component';
+import { NavbarComponent }  from './components/navbar/navbar.component';
+import { AboutComponent }  from './components/about/about.component';
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  SearchComponent,
+                  NavbarComponent,
+                  AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
