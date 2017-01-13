91a57274d25bc882f869f82b830977c755e10aeb
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-13 09:27:42 -0600

    Can now preview tracks - Finished

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,6 +9,7 @@ import { SearchComponent }  from './components/search/search.component';
 import { NavbarComponent }  from './components/navbar/navbar.component';
 import { AboutComponent }  from './components/about/about.component';
 import { ArtistComponent }  from './components/artist/artist.component';
+import { AlbumComponent }  from './components/album/album.component';
 
 import { routing } from './app.routing';
 
@@ -18,7 +19,8 @@ import { routing } from './app.routing';
                   SearchComponent,
                   NavbarComponent,
                   AboutComponent,
-                  ArtistComponent],
+                  ArtistComponent,
+                  AlbumComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -7,6 +7,7 @@ export class SpotifyService {
   private searchUrl: string;
   private artistUrl: string;
   private albumsUrl: string;
+  private albumUrl: string;
 
   constructor(private _http:Http) {
 
@@ -28,5 +29,11 @@ export class SpotifyService {
     this.albumsUrl = 'https://api.spotify.com/v1/artists/'+artistId+'/albums/';
     return this._http.get(this.albumsUrl)
       .map(res => res.json());
+  }
+
+  getAlbum(id:string) {
+  this.albumUrl = 'https://api.spotify.com/v1/albums/'+id;
+  return this._http.get(this.albumUrl)
+    .map(res => res.json());
   }
 }
